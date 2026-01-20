ee14cfee9948102d5df6796d3245cbb7262bbb3e
hmoh24 <hamsam0097@gmail.com>
2026-01-20 10:44:46 +0000

    fix toggle form section display on click

--- src/util/toggleElementVisibility.js
+++ src/util/toggleElementVisibility.js
@@ -1,9 +1,9 @@
 function toggleElementVisibility(sectionClass) {
   console.log("click");
   const element = document.getElementsByClassName(sectionClass)[0];
-  console.log(element);
-  element.style.visibility =
-    element.style.visibility === "visible" ? "hidden" : "visible";
+  console.log(element.style.display);
+  const currentDisplay = window.getComputedStyle(element).display;
+  element.style.display = currentDisplay === "none" ? "flex" : "none";
 }
 
 export default toggleElementVisibility;
